d88e834f7ff7ea2ff345c0832779e10755c507e7
maozlev <maoz20011111@gmail.com>
2021-05-24 13:20:00 +0300

    almost done

--- sources/BinaryTree.hpp
+++ sources/BinaryTree.hpp
@@ -16,10 +16,12 @@ namespace ariel{
     
         struct Node {
         T data;
-        bool is_printed = false;
+        bool is_printed_in = false;
+        bool is_printed_post = false;
         Node *left = nullptr;
         Node *right = nullptr;
-        Node(T info): data(info), left(nullptr), right(nullptr), is_printed(false){}
+        Node(T info): data(info), left(nullptr), right(nullptr), is_printed_in(false),
+        is_printed_post(false) {}
         };
         Node *root=nullptr;
 
@@ -38,7 +40,7 @@ namespace ariel{
             }
 
             BinaryTree<T> add(const T& daddy,const T& son, bool left_flag){
-                stack<Node*> s;
+                std::stack<Node*> s;
                    Node *p = root;
                    while (p != nullptr || !s.empty()){   
                        while (p !=  NULL){
@@ -72,7 +74,7 @@ namespace ariel{
 
             BinaryTree<T> add_left(const T& daddy,const T& son){
                 if(root == nullptr){
-                    throw string{"no root existing"};
+                    throw std::string{"no root existing"};
                 }
                 add(daddy, son, true);
                 return *this;
@@ -80,7 +82,7 @@ namespace ariel{
                 
             BinaryTree<T> add_right(const T& daddy,const T& son){
                 if(root == nullptr){
-                    throw string{"no root existing"};
+                    throw std::string{"no root existing"};
                 }
                 add(daddy, son, false);
                 return *this;
@@ -90,42 +92,25 @@ namespace ariel{
 
             private:
                 int Order;
-                stack<Node*> s;
-                queue<Node*> in_order_queue;
-                
+                std::stack<Node*> s;
+                std::queue<Node*> q;
                 Node* p;
 
             public:
 
-                iterator(int order, Node* ptr=nullptr):p(ptr) {
-                    Order = order;
-                    if(p!=nullptr){
-                        if(order == 0){ //preorder
-                            cout<<"in constaructor: "<<p->data<<endl;
-                            if(p->right!=nullptr){
-                                s.push(p->right);
-                            }
-                            if(p->left!=nullptr){
-                                s.push(p->left);
-                            }
+                void clean_nodes(Node* t){
+                    if(t != nullptr){
+                        t->is_printed_in = false;
+                        t->is_printed_post = false;
+                        if(t->left != nullptr){
+                            clean_nodes(t->left);
                         }
-                        else if(order == 1){ //inorder
-                            while(p != nullptr){
-                                s.push(p);
-                                p = p->left;
-                            }
-                            p = s.top();
-                            s.pop();
-                            p->is_printed = true;
-                            // cout<<"in constaructor: "<<p->data<<endl;
-                        }                            
-                        
-                        else if(order == 2){//postorder
-                            s.push(ptr);
-                        }    
+                        if(t->right != nullptr){
+                            clean_nodes(t->right);
+                        }
                     }
+                    return;      
                 }
-
                 T& operator*() const {
                     return p->data;
                 }
@@ -134,50 +119,110 @@ namespace ariel{
                     return &(p->data);
                 }
 
+                iterator(int order, Node* ptr=nullptr):p(ptr) {
+                    Order = order;
+                    if(p!=nullptr){
+                        clean_nodes(p);
+                        if(order == 0){ //preorder
+                            if(p->right!=nullptr){
+                                s.push(p->right);
+                            }
+                            if(p->left!=nullptr){
+                                s.push(p->left);
+                            }
+                        }
+                        else if(order == 1){    //inorder
+                            while(p != nullptr){
+                                s.push(p);
+                                p = p->left;
+                            }
+                            p = s.top();
+                            p->is_printed_in = true;
+                            s.pop();
+                        }                            
+                        
+                        else if(order == 2){    //postorder
+                            while(p != nullptr){
+                                s.push(p);
+                                p = p->left;
+                            }
+                            p = s.top();
+                            p->is_printed_post = true;
+                            s.pop();
+                        }    
+                    }
+                }
+
                 // pre fix ++i;
                 iterator& operator++() {
                     if(s.empty()){
                         p = nullptr;
                         return *this;
                     }
-                    if(Order == 0){ // preorder
-                    cout<<"in operator 1: "<<p->data<<endl;
-                    p = s.top();
-                    s.pop();
-                    if(p->right != nullptr){
-                        s.push(p->right);
-                    }
-                    if(p->left != nullptr){
-                        s.push(p->left);
-                    }
-                    cout<<"in operator 2: "<<p->data<<endl;
-                    return *this;
-                    }
-
-                    else if(Order == 1){ // inorder
-                    // cout<<"in operator 1: "<<p->data<<endl;
-                    p = s.top();
-                    s.pop();
+                    
+                    if(Order == 0){ // pre order
+                        p = s.top();
+                        s.pop();
                         if(p->right != nullptr){
-                            if(!p->right->is_printed){
-                                Node* tmp = p->right;
-                                s.push(p->right);
-                                p->right->is_printed = true;
-                                while(tmp->left != nullptr){
-                                    tmp = tmp->left;
-                                    s.push(tmp);
-                                    tmp->is_printed = true;
-                                }
-                            }
+                            s.push(p->right);
                         }
                         if(p->left != nullptr){
-                            if(!p->left->is_printed){
-                                s.push(p->left);
-                                p->left->is_printed = true;
-                            }
+                            s.push(p->left);
                         }
-                        
-                        p->is_printed = true;
+                        return *this;
+                    }
+
+                    else if(Order == 1){ // in order
+                        p = s.top();
+                        s.pop();
+                            if(p->right != nullptr){
+                                if(!p->right->is_printed_in){
+                                    Node* tmp = p->right;
+                                    s.push(p->right);
+                                    p->right->is_printed_in = true;
+                                    while(tmp->left != nullptr){
+                                        tmp = tmp->left;
+                                        s.push(tmp);
+                                        tmp->is_printed_in = true;
+                                    }
+                                }
+                            }
+                            if(p->left != nullptr){
+                                if(!p->left->is_printed_in){
+                                    s.push(p->left);
+                                    p->left->is_printed_in = true;
+                                }
+                            }
+                        p->is_printed_in = true;
+                        return *this;
+                    }
+                    else if(Order == 2){ // post order
+                        p = s.top();
+                        if(p->is_printed_post){
+                            s.pop();
+                        }
+                        p = s.top();
+                        s.pop();
+                        Node* tmp = p;
+                        if(p->right != nullptr && !p->right->is_printed_post){
+                            if(!p->is_printed_post){
+                                s.push(p);
+                                p->is_printed_post = true;
+                            } 
+                            s.push(p->right);
+                            p->right->is_printed_post = true;
+                            tmp = p->right;
+                        }
+                        if(p->left != nullptr && !p->left->is_printed_post){
+                            if(!p->is_printed_post){
+                                s.push(p);
+                                p->is_printed_post = true;
+                            }
+                            s.push(p->left);
+                            p->left->is_printed_post = true;
+                            tmp = p->left;
+                        }
+                        p = tmp;
                         return *this;
                     }
                     return *this;
@@ -239,7 +284,7 @@ namespace ariel{
             friend std::ostream& operator<<(std::ostream& os, const BinaryTree<T> &bt){
                 if (bt.root == NULL)
                     return os;
-                stack<Node*> s;
+                std::stack<Node*> s;
                 s.push(bt.root);
             
                 while (!s.empty()) {
